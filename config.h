1a05b2cd63ba96cee3780d7e9b9675583ffb0a68
amei <cms1984@msn.com>
2021-02-22 18:01:43 +0800

    dwm

--- config.h
+++ config.h
@@ -20,7 +20,17 @@ static const char *colors[][3]      = {
 
 /* tagging */
 /* static const char *tags[] = { "一", "二", "三", "四", "五" }; */
-static const char *tags[] = { "\uf120", "\uf7ae", "\uf121", "\uf04b", "\ue62e", "\uf251", "\ue727", "\uf537", "\uf684" };
+static const char *tags[] = { 
+	"\uf120", 
+	"\uf7ae", 
+	"\uf121", 
+	"\uf04b", 
+	//"\ue62e", 
+	//"\uf251", 
+	//"\ue727", 
+	//"\uf537", 
+	"\uf684" 
+};
 
 static const Rule rules[] = {
 	/* xprop(1):
