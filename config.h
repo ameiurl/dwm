72da45113ec09655f09786466f140dfbaa94100d
amei <cms1984@msn.com>
2024-03-24 19:15:28 +0800

    icon

--- config.h
+++ config.h
@@ -63,12 +63,13 @@ static const unsigned int alphas[][3] = {
 
 /* tagging */
 static const char *tags[] = { "", "", "", "", " ", "", "", "", "", "", "", "" };
-// static const char *tags[] = { "󰅩", "", "", "", "", "" , "  ", "  ", "  ", "  "};
+// static const char *tags[] = { "󰅩", "", "", "", "", "" , "  ", "  ", "  ", "  ",""};
 // static const char *tagsalt[] = { "󰖟", "", "", "󰙯", "", "󰎈", "󰕝", "" };
-//static const char *tags[] = { " ", " ", " ", " ", " ", " " };
+//static const char *tags[] = { " ", " ", " ", " ", " ", " ", "" };
 // static const char *tags[] = { "", "", "", "", "󰈹", "󰘬", "󰠖", "󱍢", "", "", "", ""};
 // static const char *tags[] = { "", "", "",  "", "", "","", "", ""};
 // static const char *tags[] = {"", "", "", "", "", " ", " ", "  "};
+// static const char *tags[] = { "", "", "", "", "", "", "", "", "", "" };
  
 static const Rule rules[] = {
 	/* xprop(1):
