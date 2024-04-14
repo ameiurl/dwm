860ce2a668545859f752b37f6a74c440f438f6a1
amei <cms1984@msn.com>
2024-04-14 22:53:56 +0800

    config

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray       = 1;     /* 0 means no systray */
 static const int showbar           = 1;        /* 0 means no bar */
 static const int topbar            = 0;        /* 0 means bottom bar */
-static const char *fonts[]         = { "SauceCodePro Nerd Font Mono:size=12" };
-static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=12";
+static const char *fonts[]         = { "SauceCodePro Nerd Font Mono:size=22" };
+static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=22";
 // static const char col_gray[]       = "#CCCCCC";
 // static const char col_blue[]       = "#3498DB";
 // static const char col_blue2[]      = "#0D47A1";
@@ -90,9 +90,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "",      tile },    /* first entry is default */
-	{ "",      monocle },
-	{ "",      NULL },    /* no layout function means floating behavior */
+	{ "Tile",      tile },    /* first entry is default */
+	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[M]",      monocle },
 };
 
 /* key definitions */
@@ -109,15 +109,14 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray2, "-sb", col_cyan2, "-sf", col_white, NULL };
-static const char *termcmd[]   = { "st", NULL };
-static const char *chromecmd[]  = { "google-chrome-stable", NULL };
-static const char *edgecmd[]  = { "microsoft-edge-stable", NULL };
-static const char *flameshotcmd[]  = { "flameshot", "gui", NULL };
 static const char *slockcmd[]  = { "slock", NULL };
+static const char *termcmd[]   = { "st", NULL };
+static const char *flameshotcmd[]  = { "flameshot", "gui", NULL };
+static const char *edgecmd[]  = { "microsoft-edge-stable", NULL };
+static const char *chromecmd[]  = { "google-chrome-stable", NULL };
 static const char *ranger[]    = { "st", "-e", "ranger", NULL };
 static const char *nvim[]      = { "st", "-e", "nvim", NULL };
 static const char *music[]     = { "electron-netease-cloud-music", NULL };
-static const char *flameshot[] = { "flameshot", "gui", NULL };
 static const char *lightup[] = {"xbacklight", "-inc", "10", NULL};
 static const char *lightdown[] = {"xbacklight", "-dec", "5", NULL};
 static const char *soundup[] = {"amixer", "-qM", "sset", "Master", "5%+", "unmute", NULL};
@@ -126,37 +125,36 @@ static const char *mute[] = {"amixer", "-qM", "sset", "Master", "toggle", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = slockcmd } },
+	{ MODKEY|ShiftMask,           	XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY|ShiftMask,             XK_a,	   spawn,          {.v = flameshotcmd } },
 	{ MODKEY,                       XK_e,	   spawn,          {.v = edgecmd } },
 	{ MODKEY,                       XK_c,	   spawn,          {.v = chromecmd } },
+	{ MODKEY,						XK_r,	   spawn,          {.v = ranger } },
+	{ MODKEY,						XK_n,	   spawn,          {.v = nvim } },
+	{ MODKEY|ShiftMask,				XK_e,	   spawn,          {.v = music } },
 	{ MODKEY,			            XK_Left,   spawn,	       {.v = lightup } },
 	{ MODKEY,			            XK_Right,  spawn,	       {.v = lightdown } },
 	{ MODKEY,			            XK_Up,	   spawn,	       {.v = soundup } },
 	{ MODKEY,			            XK_Down,   spawn,	       {.v = sounddown } },
 	{ MODKEY,			            XK_F8,	   spawn,	       {.v = mute } },
 	{ MODKEY|ShiftMask,             XK_f,      fullscreen,     {0} },
-	{ MODKEY|ShiftMask,           	XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,						XK_r,	   spawn,          {.v = ranger } },
-	{ MODKEY,						XK_n,	   spawn,          {.v = nvim } },
-	{ MODKEY|ShiftMask,				XK_e,	   spawn,          {.v = music } },
-	{ MODKEY,						XK_F1,	   spawn,          {.v = flameshot } },
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_h,      incnmaster,     {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_l,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_u,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	// { MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
+	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
