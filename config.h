2d120c19ff4d53df00f09a49da84a39b2f0a40a2
amei <cms1984@msn.com>
2025-05-08 08:51:23 +0000

    st tmux

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray       = 1;     /* 0 means no systray */
 static const int showbar           = 1;        /* 0 means no bar */
 static const int topbar            = 0;        /* 0 means bottom bar */
-static const char *fonts[]         = { "SauceCodePro Nerd Font Mono:size=12" };
-static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=12";
+static const char *fonts[]         = { "SauceCodePro Nerd Font Mono:size=18" };
+static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=18";
 // static const char col_gray[]       = "#CCCCCC";
 // static const char col_blue[]       = "#3498DB";
 // static const char col_blue2[]      = "#0D47A1";
@@ -109,7 +109,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray2, "-sb", col_cyan2, "-sf", col_white, NULL };
-static const char *termcmd[]   = { "st", NULL };
+// static const char *termcmd[]   = { "st", NULL };
+static const char *termcmd[]    = { "st", "-e", "tmux", NULL };
 static const char *chromecmd[]  = { "google-chrome-stable", NULL };
 static const char *edgecmd[]  = { "microsoft-edge-stable", NULL };
 static const char *flameshotcmd[]  = { "flameshot", "gui", NULL };
